6c5a2a8801f57ecb77ea2a83b917210a84fb3fd3
wxmetvis/NAWDEX <wxmetvis@posteo.de>
2016-09-23 18:10:10 +0000

    disabled trajectory bbox filter as it doesn't work with p(WCB) modules; some alternative pipeline config is required

--- src/system/pipelineconfiguration.cpp
+++ src/system/pipelineconfiguration.cpp
@@ -469,18 +469,18 @@ void MPipelineConfiguration::initializeLagrantoEnsemblePipeline(
     dpdtFilter->setInputSelectionSource(thinoutFilter);
     dpdtFilter->setDeltaPressureSource(dpSource);
 
-    MBoundingBoxTrajectoryFilter *bboxFilter =
-            new MBoundingBoxTrajectoryFilter();
-    bboxFilter->setMemoryManager(memoryManager);
-    bboxFilter->setScheduler(scheduler);
-    bboxFilter->setInputSelectionSource(dpdtFilter);
-    bboxFilter->setTrajectorySource(trajectoryReader);
+//    MBoundingBoxTrajectoryFilter *bboxFilter =
+//            new MBoundingBoxTrajectoryFilter();
+//    bboxFilter->setMemoryManager(memoryManager);
+//    bboxFilter->setScheduler(scheduler);
+//    bboxFilter->setInputSelectionSource(dpdtFilter);
+//    bboxFilter->setTrajectorySource(trajectoryReader);
 
     MSingleTimeTrajectoryFilter *timestepFilter =
             new MSingleTimeTrajectoryFilter();
     timestepFilter->setMemoryManager(memoryManager);
     timestepFilter->setScheduler(scheduler);
-    timestepFilter->setInputSelectionSource(bboxFilter);
+    timestepFilter->setInputSelectionSource(dpdtFilter);
     sysMC->registerDataSource(dataSourceId + QString(" timestepFilter"),
                               timestepFilter);
 
