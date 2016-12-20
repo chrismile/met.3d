96148cddec5507457830be9ce9156bec203d5f61
Bianca Tost <bianca.tost@mytum.de>
2016-12-20 08:51:02 +0100

    added support for textured filled contours to horizontal section actor

---
+++ src/actors/spatial1dtransferfunction.h
@@ -0,0 +1,196 @@
+/******************************************************************************
+**
+**  This file is part of Met.3D -- a research environment for the
+**  three-dimensional visual exploration of numerical ensemble weather
+**  prediction data.
+**
+**  Copyright 2016 Marc Rautenhaus
+**  Copyright 2016 Bianca Tost
+**
+**  Computer Graphics and Visualization Group
+**  Technische Universitaet Muenchen, Garching, Germany
+**
+**  Met.3D is free software: you can redistribute it and/or modify
+**  it under the terms of the GNU General Public License as published by
+**  the Free Software Foundation, either version 3 of the License, or
+**  (at your option) any later version.
+**
+**  Met.3D is distributed in the hope that it will be useful,
+**  but WITHOUT ANY WARRANTY; without even the implied warranty of
+**  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+**  GNU General Public License for more details.
+**
+**  You should have received a copy of the GNU General Public License
+**  along with Met.3D.  If not, see <http://www.gnu.org/licenses/>.
+**
+*******************************************************************************/
+#ifndef MSPATIAL1DTRANSFERFUNCTION_H
+#define MSPATIAL1DTRANSFERFUNCTION_H
+
+// standard library imports
+#include <memory>
+
+// related third party imports
+#include <GL/glew.h>
+#include <QtProperty>
+#include <QtCore>
+
+// local application imports
+#include "gxfw/transferfunction.h"
+#include "gxfw/gl/shadereffect.h"
+#include "gxfw/gl/texture.h"
+
+class MGLResourcesManager;
+class MSceneViewGLWidget;
+
+
+namespace Met3D
+{
+
+
+class MSpatial1DTransferFunction : public MTransferFunction
+{
+public:
+    MSpatial1DTransferFunction();
+    ~MSpatial1DTransferFunction();
+
+    void reloadShaderEffects();
+
+    void saveConfiguration(QSettings *settings);
+
+    void loadConfiguration(QSettings *settings);
+
+    QString getSettingsID() override { return "Spatial1DTransferFunction"; }
+
+    /**
+      Returns the texture that represents the spatial transfer function texture.
+      */
+    GL::MTexture* getTexture() { return tfTexture; }
+
+    /**
+      Returns the minimum scalar value of the current transfer function
+      settings.
+      */
+    float getMinimumValue() { return minimumValue; }
+
+    /**
+      Returns the maximum scalar value of the current transfer function
+      settings.
+      */
+    float getMaximumValue() { return maximumValue; }
+
+    float getNumLevels() { return numLevels; }
+
+    bool getClampMaximum() { return clampMaximum; }
+
+    float getInterpolationRange() { return interpolationRange; }
+
+    float getTextureScale() { return (float)textureScale; }
+
+    float getTextureAspectRatio()
+    { return (float)currentTextureHeight / (float)currentTextureWidth; }
+
+    void setMinimumValue(float value);
+
+    void setMaximumValue(float value);
+
+    void setValueDecimals(int decimals);
+
+    void setPosition(QRectF position);
+
+    QString transferFunctionName();
+
+protected:
+    void initializeActorResources();
+
+    void onQtPropertyChanged(QtProperty *property);
+
+    void renderToCurrentContext(MSceneViewGLWidget *sceneView);
+
+private:
+    /**
+      Generates the colourbar texture with the colour mapping specified by the
+      user and uploads a 1D-texture to the GPU.
+
+      @see MColourmapPool
+      @see MColourmap
+      */
+    void generateTransferTexture(int level, bool recreate);
+
+    /**
+      Creates geometry for a box filled with the colourbar texture and for tick
+      marks, and places labels at the tick marks.
+      */
+    void generateTextureBarGeometry();
+
+    void loadImagesFromPaths(QStringList pathList);
+
+    std::shared_ptr<GL::MShaderEffect> simpleGeometryShader;
+    std::shared_ptr<GL::MShaderEffect> colourbarShader;
+
+    GL::MTexture *tfTexture;
+    GLint textureUnit;
+
+    GL::MVertexBuffer *vertexBuffer;
+    uint numVertices;
+
+    QVector<QImage> loadedImages;
+
+    // General properties.
+    QtProperty *positionProperty;
+
+    // Properties related to ticks and labels.
+    QtProperty *maxNumTicksProperty;
+    uint        numTicks;
+    QtProperty *maxNumLabelsProperty;
+    QtProperty *tickWidthProperty;
+    QtProperty *labelSpacingProperty;
+    QtProperty *scaleFactorProperty;
+
+    // Properties related to texture levels.
+    QtProperty          *levelsPropertiesSubGroup;
+    int                  numLevels;
+    QtProperty          *loadLevelsImagesProperty;
+    QtProperty          *pathToLoadedImagesProperty;
+    QVector<QString>     pathsToLoadedImages;
+    QString              imagePathsString;
+
+
+    // Properties related to value range.
+    QtProperty *rangePropertiesSubGroup;
+    QtProperty *valueDecimalsProperty;
+    QtProperty *minimumValueProperty;
+    QtProperty *maximumValueProperty;
+    double      minimumValue;
+    double      maximumValue;
+    QtProperty *clampMaximumProperty;
+    bool        clampMaximum;
+    QtProperty *interpolationRangeProperty;
+    double      interpolationRange; // Defines scalar range of interpolated texture.
+
+    // Properties related to texture scale.
+    QtProperty *textureScalePropertiesSubGroup;
+    QtProperty *textureScaleDecimalsProperty;
+    QtProperty *textureScaleProperty;
+    // Scale of texture width with respect in longitude. Texture height is
+    // scaled keeping the aspect ratio.
+    double      textureScale;
+
+    int         currentTextureWidth;
+    int         currentTextureHeight;
+};
+
+
+class MSpatial1DTransferFunctionFactory : public MAbstractActorFactory
+{
+public:
+    MSpatial1DTransferFunctionFactory() : MAbstractActorFactory() {}
+
+protected:
+    MActor* createInstance() override { return new MSpatial1DTransferFunction(); }
+};
+
+
+} // namespace Met3D
+
+#endif // MSPATIAL1DTRANSFERFUNCTION_H
--- src/glsl/colourbar.fx.glsl
+++ src/glsl/colourbar.fx.glsl
@@ -4,7 +4,8 @@
 **  three-dimensional visual exploration of numerical ensemble weather
 **  prediction data.
 **
-**  Copyright 2015 Marc Rautenhaus
+**  Copyright 2015-2016 Marc Rautenhaus
+**  Copyright 2015-2016 Bianca Tost
 **
 **  Computer Graphics and Visualization Group
 **  Technische Universitaet Muenchen, Garching, Germany
@@ -30,6 +31,7 @@
 
 interface VStoFS
 {
+    smooth vec2 texCoords;
     smooth float texCoord;
 };
 
@@ -42,6 +44,9 @@ shader VSmain(in vec3 vertexCoord : 0, in vec3 texCoordIn : 1, out VStoFS Output
 {
     gl_Position = vec4(vertexCoord, 1);
     Output.texCoord = texCoordIn.r;
+
+    Output.texCoords.x = texCoordIn.g; // float(gl_VertexID > 1);
+    Output.texCoords.y = texCoordIn.r;
 }
 
 
@@ -59,6 +64,87 @@ shader FSmain(in VStoFS Input, out vec4 fragColour)
 }
 
 
+uniform sampler3D spatialTransferTexture;
+
+uniform float distInterp; // user
+
+uniform float minimumValue;
+uniform float maximumValue;
+
+uniform int numLevels; // Amount of textures loaded.
+
+uniform int viewPortWidth;
+uniform int viewPortHeight;
+
+uniform int textureWidth;
+uniform int textureHeight;
+
+uniform float barWidthF;
+uniform float barHeightF;
+
+shader FSspatialTF(in VStoFS Input, out vec4 fragColour)
+{
+    // Float versions of the given sizes.
+    float viewPortWidthF = float(viewPortWidth);
+    float viewPortHeightF = float(viewPortHeight);
+
+    float textureWidthF = float(textureWidth);
+    float textureHeightF = float(textureHeight);
+
+    // Adapt to change scale of texture in colour bar.
+    vec2 scale = vec2((textureWidthF / viewPortWidthF) * (2.0f / barWidthF),
+                      (textureHeightF / viewPortHeightF) * (2.0f / barHeightF));
+
+    float dist = 1.0f / float(numLevels);
+
+    vec2 pos = Input.texCoords;
+
+    // Get level position in 3D texture. Doesn't start with first image at 0.0f,
+    // but at 1/(2*numLevels).
+    float level =
+            ((floor(pos.y / dist) * 2.0f) + 1.0f) / float(2.0f * numLevels);
+
+    vec3 texCoords = vec3(fract(pos / scale), level);
+
+    vec3 colour = textureLod(spatialTransferTexture, texCoords, 0.0f).rgb;
+
+    // Interpolation range in texture coordinates.
+    float interpRange = (distInterp / 2.0f) / (maximumValue - minimumValue);
+    // Scalar value distance to current level in texture coordinates.
+    float fraction = pos.y - (floor(pos.y / dist) * dist);
+    // Scalar value distance to next level in texture coordinates.
+    float fraction2 = (ceil(pos.y / dist) * dist) - pos.y;
+
+    // Interpolation to lower level. (No interpolation for lowest level!)
+    if (interpRange > fraction && floor(pos.y / dist) > 0.0f)
+    {
+        level = (((floor(pos.y / dist) - 1.f) * 2.0f) + 1.0f)
+                / float(2.0f * numLevels);
+        float interpolation = fraction / (2.0f * interpRange) + 0.5f;
+
+        texCoords = vec3(fract(pos / scale), level);
+
+        colour = (interpolation * colour)
+                + ((1.f - interpolation)
+                   * textureLod(spatialTransferTexture, texCoords, 0.0f).rgb);
+    }
+    // Interpolation to upper level. (No interpolation for highest level!)
+    else if (fraction2 < interpRange
+             && floor(pos.y / dist) < float(numLevels - 1))
+    {
+        level = ((ceil(pos.y / dist) * 2.0f) + 1.0f) / float(2.0f * numLevels);
+        float interpolation = fraction2 / (2.0f * interpRange) + 0.5f;
+
+        texCoords = vec3(fract(pos / scale), level);
+
+        colour = (interpolation * colour)
+                + ((1.f - interpolation)
+                   * textureLod(spatialTransferTexture, texCoords, 0.0f).rgb);
+    }
+
+    fragColour = vec4(colour, 1.0f);
+}
+
 /*****************************************************************************
  ***                             PROGRAMS
  *****************************************************************************/
@@ -67,4 +153,11 @@ program Standard
 {
     vs(330)=VSmain();
     fs(330)=FSmain();
+};
+
+program spatialTF
+{
+    vs(330)=VSmain();
+//    fs(330)=FSleveled();
+    fs(330)=FSspatialTF();
 };
