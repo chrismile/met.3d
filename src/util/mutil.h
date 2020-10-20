58c508f1f02577a1c1d032f8c8e4b5458c9bbbaa
Marc Rautenhaus <wxmetvis@posteo.de>
2020-10-20 12:17:53 +0200

    difference data source: added functionality to compute tendencies (i.e. difference of data field to different time step)

--- src/util/mutil.h
+++ src/util/mutil.h
@@ -47,7 +47,7 @@ const QString met3dVersionBranchID = "";
 // Set to "-devel" for development versions.
 const QString met3dVersionDevelID = "";
 
-const QString met3dVersionString = "1.6/20.10.5" + met3dVersionDevelID
+const QString met3dVersionString = "1.6/20.10.6" + met3dVersionDevelID
         + met3dVersionBranchID;
 // String containing default value for missing version number in config.
 const QString defaultConfigVersion = "1.0.0";
